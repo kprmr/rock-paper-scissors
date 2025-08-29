83e5c500406c8b766ede5c38ae3fbfa533ab7e5b
kprmr <parmarskush@gmail.com>
2025-08-29 11:23:03 +0530

    Added rules to be displayed in console in index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(`Welcome!
+Rules: 
+Please select one between 'Rock', 'Paper' and 'Scissor' and type in the alert box given.
+You can type in lowercase or uppercase.
+You can not enter anything other then the three choices mentioned.
+Good Luck`)
