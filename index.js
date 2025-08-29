bcdb8d4d694dca3b5452fa1bfb5f34a871f64c08
kprmr <parmarskush@gmail.com>
2025-08-29 23:41:33 +0530

    Added playRound function
    It compares the values of human score and computer score and give the verdict who won and how.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log(`Welcome!
 Rules: 
 Please select one between 'Rock', 'Paper' and 'Scissor' and type in the alert box given.
 You can not enter anything other then the three choices mentioned.
-Good Luck`)
+Good Luck`);
 // ---------------------------------------------------------------
 //The real coding start here
 // This function return computer's choice of rock, paper or scissor
@@ -23,4 +23,48 @@ function getHumanChoice(){
     let humanChoice = humanChoicePrompt.toLowerCase()
     return humanChoice
 }
+
+//Declaration of Score variable which will help in keeping track of scores through the game
+let humanScore = 0;
+let computerScore = 0;
+
+/*
+1. It take the value of human and computer
+2. It compare the value 
+3. Determine the winner based on rock, paper and scissor rules
+4. Give notification (One of these)
+    a. You Lose! Paper beats Rock
+    b. You Lose! Scissor beats Paper
+    c. You Lose! Rock beats Scissor
+    d. You Win! Paper beats Rock
+    e. You Win! Scissor beats Paper
+    f. You Win! Rock beats Scissor
+    g. It's a Draw!
+*/
+//This function takes human and computer's choice as argument and give the score of round and who won
+function playRound(x, y){
+    //Black Box
+    if (x == y) {
+        return "It's a Draw!"
+    }
+    else if (x == "paper" && y == "rock") {
+        return "You lose! Paper beats Rock"
+    }
+    else if (x == "scissor" && y == "paper") {
+        return "You Lose! Scissor beats Paper"
+    }
+    else if (x == "rock" && y == "scissor") {
+        return "You Lose! Rock beats Scissor"
+    }
+    else if (x == "rock" && y == "paper") {
+        return "You Win! Paper beats Rock"
+    }
+    else if (x == "paper" && y == "scissor") {
+        return "You Win! Scissor beats Paper"
+    }
+    else if (x == "scissor" && y == "rock") {
+        return "You Win! Rock beats scissor"
+    }
+}
+
 
