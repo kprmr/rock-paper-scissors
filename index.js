d3ff3b253a4f1fe0a1230906bfa322f2c892e9d0
kprmr <parmarskush@gmail.com>
2025-08-29 23:07:21 +0530

    Made getComputerChoice function
    This funciton give a random string between Rock, Paper and Scissor

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 console.log(`Welcome!
 Rules: 
 Please select one between 'Rock', 'Paper' and 'Scissor' and type in the alert box given.
-You can type in lowercase or uppercase.
 You can not enter anything other then the three choices mentioned.
 Good Luck`)
+// ---------------------------------------------------------------
+//The real coding start here
+// To get computer choice 
+/*
+1. the function getComputerChoice randomly returns rock, paper or scissor
+for that 
+we can use math.random function this function returns value between 0 and 1
+2. so we can split 0 to 1 to 3 parts 0 to 0.33, 0.33 to 0.66 and 0.66 to 0.99
+    which will give rock, paper and scissor respectively
+*/
+/*
+step one randomly select number between 1 to 100
+step 2 make it so that 0 to 33 is rock, 33 to 66 is paper and 66 to 99 is scissor
+step 3 display the random string
+*/
+function getComputerChoice(){
+    let computerChoiceNumber = Math.floor(Math.random() * 100) 
+    if (computerChoiceNumber <= 33 && computerChoiceNumber >= 0) {
+        return "Rock"
+    }
+    else if (computerChoiceNumber <= 66 && computerChoiceNumber > 33) {
+        return "Paper"
+    }
+    else if (computerChoiceNumber <= 99 && computerChoiceNumber > 66) {
+        return "Scissor"
+    }
+}
+
+console.log(getComputerChoice())
