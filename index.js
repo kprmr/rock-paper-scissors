0de46a4bbd8790ef7c8e917ed2dcc3e8589b8967
kprmr <parmarskush@gmail.com>
2025-08-29 23:21:20 +0530

    Added getHumanChoice function

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ You can not enter anything other then the three choices mentioned.
 Good Luck`)
 // ---------------------------------------------------------------
 //The real coding start here
-// To get computer choice 
-/*
-1. the function getComputerChoice randomly returns rock, paper or scissor
-for that 
-we can use math.random function this function returns value between 0 and 1
-2. so we can split 0 to 1 to 3 parts 0 to 0.33, 0.33 to 0.66 and 0.66 to 0.99
-    which will give rock, paper and scissor respectively
-*/
-/*
-step one randomly select number between 1 to 100
-step 2 make it so that 0 to 33 is rock, 33 to 66 is paper and 66 to 99 is scissor
-step 3 display the random string
-*/
+// This function return computer's choice of rock, paper or scissor
 function getComputerChoice(){
     let computerChoiceNumber = Math.floor(Math.random() * 3)
     if (computerChoiceNumber === 0) {
@@ -29,5 +17,10 @@ function getComputerChoice(){
     else return "Scissor"
 }
 
+//This function return the human's choice of rock, paper or scissor
+function getHumanChoice(){
+    let humanChoicePrompt = prompt("What will you choose :- Rock, Paper or Scissor?")
+    let humanChoice = humanChoicePrompt.toLowerCase()
+    return humanChoice
+}
 
-console.log(getComputerChoice())
