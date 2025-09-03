c25c24bd3dda6171ebe75283d7d3c955962a45d8
kprmr <parmarskush@gmail.com>
2025-09-03 19:06:33 +0530

    Added Buttons and made some logic changes
    Deleted playGame function and changed the playRound function so that it could work with button's EvenListener on click.

--- index.js
+++ index.js
@@ -5,93 +5,54 @@ You can not enter anything other then the three choices mentioned.
 Good Luck`);
 // ---------------------------------------------------------------
 //The real coding start here
-// This function return computer's choice of rock, paper or scissor
-function getComputerChoice(){
-    let computerChoiceNumber = Math.floor(Math.random() * 3)
-    if (computerChoiceNumber === 0) {
-        return "Rock".toLowerCase()
-    }
-    else if (computerChoiceNumber === 1) {
-        return "Paper".toLowerCase()
-    }
-    else return "Scissor".toLowerCase()
-}
 
-//This function return the human's choice of rock, paper or scissor
-function getHumanChoice(){
-    let humanChoicePrompt = prompt(`What will you choose :- Rock, Paper or Scissor?
-And Please open the console window in developer tools`)
-    let humanChoice = humanChoicePrompt.toLowerCase()
-    return humanChoice
-}
-
-//This function calls playRound to run 5 times, keeps track of score and declare the winner in end.
-function playGame(){
-    //Declaration of Score variable which will help in keeping track of scores through the game
-    let humanScore = 0;
-    let computerScore = 0;
-
-    //This function takes human and computer's choice as argument and give the score of round and who won
-    function playRound(x, y){
-        if (x == y) {
-            return "It's a Draw!"
+//This function takes human's choice as argument and give the score of round and who won
+function playRound(x){
+    // This function return computer's choice of rock, paper or scissor
+    function getComputerChoice(){
+        let computerChoiceNumber = Math.floor(Math.random() * 3)
+        if (computerChoiceNumber === 0) {
+           return "Rock"
         }
-        else if (x == "paper" && y == "rock") {
-            return "You Lose! Paper beats Rock."
+        else if (computerChoiceNumber === 1) {
+            return "Paper"
         }
-        else if (x == "scissor" && y == "paper") {
-            return "You Lose! Scissor beats Paper."
-        }
-        else if (x == "rock" && y == "scissor") {
-            return "You Lose! Rock beats Scissor."
-        }
-        else if (x == "rock" && y == "paper") {
-            return "You Win! Paper beats Rock."
-        }
-        else if (x == "paper" && y == "scissor") {
-            return "You Win! Scissor beats Paper."
-        }
-        else if (x == "scissor" && y == "rock") {
-            return "You Win! Rock beats Scissor."
-        }
+        else return "Scissors"
     }
 
-    //This loop will invoke playRound function and the score keeping/incrementing five times
-    for(let i = 0; i<5; i++){
+    let y = getComputerChoice()
 
-        //This lines give the values of getHumanChoice and getComputerChoice function and assign them to constants
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        //The assigned constants are passed as an argument of playRound function and the return value is stored in result constant
-        const result = playRound(computerSelection, humanSelection);
-
-        //Increment the score if someone wins
-        if (result.includes("Win")) {
-            humanScore = humanScore + 1
-        }
-        if (result.includes("Lose")){
-            computerScore = computerScore + 1
-        }
-
-        console.log(result)
-        console.log(`The scores are:- You:${humanScore}, Computer:${computerScore}`);
-        // console.log(computerScore);
-        // console.log(humanScore);
+    if (x == y) {
+        return "It's a Draw!"
     }
-    if (humanScore > computerScore) {
-        console.log(`
-            You are the final WINNER!
-            Final Score:- You:${humanScore}, Computer:${computerScore}`);
+    else if (x == "Paper" && y == "Rock") {
+        return "You Lose! Paper beats Rock."
     }
-    else if (humanScore < computerScore) {
-        console.log(`
-            Computer is the final WINNER and You are LOSER!
-            Final Score:- You:${humanScore}, Computer:${computerScore}`);
+    else if (x == "Scissors" && y == "Paper") {
+        return "You Lose! Scissors beats Paper."
     }
-    else {
-        console.log("BAH! It was a draw.");
+    else if (x == "Rock" && y == "Scissors") {
+        return "You Lose! Rock beats Scissors."
+    }
+    else if (x == "Rock" && y == "Paper") {
+        return "You Win! Paper beats Rock."
+    }
+    else if (x == "Paper" && y == "Scissors") {
+        return "You Win! Scissors beats Paper."
+    }
+    else if (x == "Scissors" && y == "Rock") {
+        return "You Win! Rock beats Scissors."
     }
 }
 
-playGame();
+//This selects the buttons to be chosen
+const buttons = document.querySelectorAll('.btn');
+
+// Registering a click event in buttons and passing that to playRound function
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const humanChoice = e.target.innerHTML
+        console.log(playRound(humanChoice))
+    });
+});
+
